3dc19e70e75f5fcd40207b905920cd3acd62167b
VirxEC <virx@virxcase.dev>
2022-05-17 12:07:19 -0400

    More stats

--- src/App.java
+++ src/App.java
@@ -1,5 +1,4 @@
-//import java.util.concurrent.TimeUnit;
-
+import java.util.Map;
 import java.util.List;
 import java.util.Scanner;
 
@@ -8,27 +7,89 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+// import commands.Ping;
+// import net.dv8tion.jda.api.JDA;
+// import net.dv8tion.jda.api.JDABuilder;
+// import net.dv8tion.jda.api.entities.Guild;
+// import net.dv8tion.jda.api.interactions.commands.OptionType;
+// import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
+// import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
+
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\ericm\\OneDrive\\Documents\\selenium-java\\bin\\chromedriver.exe");
+        Map<String, String> env = System.getenv();
+        System.setProperty("webdriver.chrome.driver", env.get("CHROMEDRIVER_PATH"));
 
-        // get line of text from user
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Search Query:");
-        String line = scanner.nextLine();
-        //close scanner
-        scanner.close();
-
-        WebDriver driver = new ChromeDriver();
-        //Auto converts symbols to encode for URL and makes sure it is a valid URL
-        driver.get("https://www.youtube.com/results?search_query=" + line);
-        //WebElement titles = driver.findElement(By.className("ytd-video-renderer"));
-        List<WebElement> titles = driver.findElements(By.cssSelector("yt-formatted-string.ytd-video-renderer"));
-  
-        //This is the title of the first video
-        System.out.println(titles.get(0).getText());
-        driver.quit();   
+        // JDA api = JDABuilder.createDefault(env.get("JAVACORD_TOKEN")).build();
+        // api.addEventListener(new Ping());
         
+        // api.awaitReady();
+        // Guild guild = api.getGuildById("900193901959315517");
+        
+        // guild.upsertCommand("ping", "pongs").addSubcommands({new Command().setName("title").setDescription("Gets the title of a youtube video")});
 
+        
+        //starting the actual bot commands
+
+        // selenium stuff
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.youtube.com");
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            // get line of text from user
+            System.out.println("Search Query:");
+            String line = "";
+            //close scanner
+
+            while (line.equals("")) {
+                line = scanner.nextLine();
+            }
+            
+            if (line.equals("exit")) {
+                break;
+            }
+            
+            //Auto converts symbols to encode for URL and makes sure it is a valid URL
+            driver.get("https://www.youtube.com/results?search_query=" + line);
+            
+            //This is the title of the first video
+            List<WebElement> titles = driver.findElements(By.cssSelector("yt-formatted-string.ytd-video-renderer"));
+            
+            if (titles.size() == 0) {
+                System.out.println("No videos found");
+                continue;
+            }
+
+            // Get video meta data
+            List<WebElement> meta = driver.findElements(By.cssSelector("span.ytd-video-meta-block"));
+
+            // Get video views
+            // System.out.println(meta.get(0).getText());
+            // // Get time posted
+            // System.out.println(meta.get(1).getText());
+
+            System.out.println("\nTop results");
+
+            System.out.println("1. " + titles.get(0).getText());
+            System.out.println("2. " + titles.get(4).getText());
+            System.out.println("3. " + titles.get(8).getText());
+
+            System.out.println("\nType in the number of the corresponding video");
+            int videoNum = scanner.nextInt() - 1;
+
+            System.out.println(meta.get(videoNum * 2).getText());
+            System.out.println(meta.get(videoNum * 2 + 1).getText());
+
+            
+            List<WebElement> links = driver.findElements(By.cssSelector("a.yt-simple-endpoint.style-scope.ytd-video-renderer"));
+            System.out.println(links.get(0).getAttribute("href"));
+            System.out.println(links.get(2).getAttribute("href"));
+            System.out.println(links.get(4).getAttribute("href"));
+        }
+        
+        scanner.close();
+        driver.quit();   
     }
 }
---
+++ src/Scraper.java
@@ -0,0 +1,63 @@
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class Scraper {
+    private WebDriver driver;
+
+    public Scraper(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public boolean searchVideo(String searchQuery) {
+        driver.get("https://www.youtube.com/results?search_query=" + searchQuery);
+        try{        
+            driver.findElement(By.cssSelector("yt-formatted-string.ytd-video-renderer"));
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public ArrayList<String> getVideoTitles() {
+        ArrayList<String> titles = new ArrayList<String>();
+        List<WebElement> elements = driver.findElements(By.cssSelector("yt-formatted-string.ytd-video-renderer"));
+        for (int i = 0; i < elements.size(); i+=4) {
+            titles.add(elements.get(i).getText());
+        }
+        return titles;
+    }
+
+    public ArrayList<String> getTop5(){
+        ArrayList<String> titles = getVideoTitles();
+        ArrayList<String> top5 = new ArrayList<String>();
+        for (int i = 0; i < 5; i++) {
+            if(titles.size() > i) {
+                top5.add(titles.get(i));
+            } else{
+                break;
+            }
+        }
+        return top5;
+    }
+
+    public ArrayList<String> getMeta(int i){
+        ArrayList<String> meta = new ArrayList<String>();
+        List<WebElement> elements = driver.findElements(By.cssSelector("yt-formatted-string.ytd-video-renderer"));
+        meta.add(elements.get(i * 2).getText());
+        meta.add(elements.get(i * 2 + 1).getText());
+        List<WebElement> links = driver.findElements(By.cssSelector("a.yt-simple-endpoint.style-scope.ytd-video-renderer"));
+        meta.add(links.get(i).getAttribute("href"));
+        return meta;
+    }
+
+ }
+    
+
